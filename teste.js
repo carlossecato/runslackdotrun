2160cb5ca3fbe113a9262fb48814acefd23bb73c
carlossecato <carlos.secato@gmail.com>
2020-03-07 17:54:31 -0300

    db_connection-init

--- teste.js
+++ teste.js
@@ -4,7 +4,7 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
-
+var auth = require('./auth');
 
 app.use(express.static(__dirname + "/"))
 
@@ -20,7 +20,9 @@ wss.on("connection", function(ws) {
   var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)
-
+app.get('/auth', function(req,res){
+  res.send(auth)
+})
 
   console.log("websocket connection open")
 
